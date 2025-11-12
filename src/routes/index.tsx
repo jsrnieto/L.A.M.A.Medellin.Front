cc80294e8a4560ead3c7fab43fc6418c050c50e8
JUAN SEBASTIAN RESTREPO NIETO <jsrnieto@crystal.com.co>
2025-11-12 18:48:09 -0500

    Rutas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,27 +3,29 @@ import Home from "../components/pages/Home";
 import Login from "../components/pages/Login";
 import Members from "../components/pages/Members";
 import NotFound from "../components/pages/NotFound";
+import Statistics from "../components/pages/Statistics";
 
-
-const IndexRouter = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/login",
-      element: <Login />,
-    },
-    {
-      path: "/members",
-      element: <Members />,
-    },
-    {
-      path: "*",
-      element: <NotFound />,
-    }
-  ],
-);
+const IndexRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/members",
+    element: <Members />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+  {
+    path: "/statistics",
+    element: <Statistics />,
+  },
+]);
 
 export default IndexRouter;
